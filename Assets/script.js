0057c125a5b92291d2ef1945b3728590022f0068
Dmitri So <dmitriso@yahoo.com>
2020-12-09 22:33:41 -0500

    Created color coding  for UV Index.

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,3 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-// WHEN I open the weather dashboard
-// THEN I am presented with the last searched city forecast
-
 var key = "84eba78b970b65a9aa20f76723c97703";
 var degree = "\u02DA";
 var cityList = [];
@@ -35,7 +21,7 @@ $(document).ready(function () {
 
         // if (cityName === "") {
         //     cityName = cityList[0];
-            
+
         // }
 
         for (var i = 0; i < cityList.length; i++) {
@@ -61,86 +47,95 @@ $(document).ready(function () {
     $("#searchCity").on("click", function (event) {
         event.preventDefault();
         // local variables for method use
-        
-            var cityName = $("#newCity").val();
-            var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + key;
+
+        var cityName = $("#newCity").val();
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + key;
 
 
-            // This pushes the new city into the city list array
-            cityList.push(cityName);
-            // This method pulls the lat and lon of new city to be used 
+        // This pushes the new city into the city list array
+        cityList.push(cityName);
+        // This method pulls the lat and lon of new city to be used 
+        $.ajax({
+            url: queryURL,
+            method: "Get"
+        }).then(function (res) {
+            console.log(res);
+            lat = res.coord.lat;
+            lon = res.coord.lon;
+
+            var lat;
+            var lon;
+            var gpsURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=" + "&appid=" + key;
+
+            // This uses the lat and lon pulled from the above method to call desired weather data
             $.ajax({
-                url: queryURL,
+                url: gpsURL,
                 method: "Get"
-            }).then(function (res) {
-                console.log(res);
-                lat = res.coord.lat;
-                lon = res.coord.lon;
+            }).then(function (response) {
+                console.log(response);
+                var iconCode = response.current.weather[0].icon;
+                var iconURL = "http://openweathermap.org/img/wn/" + iconCode + ".png";
+                var fahrenheit = Math.round(((parseFloat(res.main.temp) - 273.15) * 1.80 + 32));
+                var celcius = Math.round(parseFloat(res.main.temp) - 273.15);
+                // Displays data onto designated elements
+                $("#currentCity").text(res.name + "|" + (moment().format(" MMM. Do YY")));
+                $("#description").text("Clouds: " + res.weather[0].description);
+                $("#humidity").text("Humidity: " + res.main.humidity + "%");
+                $("#windSpeed").text("Wind Speed: " + res.wind.speed);
+                $("#fahrenheit").text("Temp: " + fahrenheit + degree + "F");
+                $("#celcius").text("Temp: " + celcius + degree + "C");
+                // Displaying data from second object
+                $("#uv").text("UV Index: " + response.current.uvi);
+                $("#wicon").attr("src", iconURL);
 
-                var lat;
-                var lon;
-                var gpsURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=" + "&appid=" + key;
+                // This Displays 5 day forecast for the week
+                // This displays upcoming dates
+                $("#date1").text(moment().add(1, 'days').format("MMM Do YY"));
+                $("#date2").text(moment().add(2, 'days').format("MMM Do YY"));
+                $("#date3").text(moment().add(3, 'days').format("MMM Do YY"));
+                $("#date4").text(moment().add(4, 'days').format("MMM Do YY"));
+                $("#date5").text(moment().add(5, 'days').format("MMM Do YY"));
+                // Forecast for temp
+                $("#temp1").text("Temperature: " + Math.round(((response.daily[0].temp.day) - 273.15) * 1.80 + 32) + degree);
+                $("#temp2").text("Temperature: " + Math.round(((response.daily[1].temp.day) - 273.15) * 1.80 + 32) + degree);
+                $("#temp3").text("Temperature: " + Math.round(((response.daily[2].temp.day) - 273.15) * 1.80 + 32) + degree);
+                $("#temp4").text("Temperature: " + Math.round(((response.daily[3].temp.day) - 273.15) * 1.80 + 32) + degree);
+                $("#temp5").text("Temperature: " + Math.round(((response.daily[4].temp.day) - 273.15) * 1.80 + 32) + degree);
+                // Forecast for humidity
+                $("#humidity1").text("Humidity: " + response.daily[0].humidity + "%");
+                $("#humidity2").text("Humidity: " + response.daily[1].humidity + "%");
+                $("#humidity3").text("Humidity: " + response.daily[2].humidity + "%");
+                $("#humidity4").text("Humidity: " + response.daily[3].humidity + "%");
+                $("#humidity5").text("Humidity: " + response.daily[4].humidity + "%");
+                // This displays the weather icon for upcoming day
+                // URLS to one call open weather api
+                var iconURL0 = "http://openweathermap.org/img/wn/" + response.daily[0].weather[0].icon + ".png";
+                var iconURL1 = "http://openweathermap.org/img/wn/" + response.daily[1].weather[0].icon + ".png";
+                var iconURL2 = "http://openweathermap.org/img/wn/" + response.daily[2].weather[0].icon + ".png";
+                var iconURL3 = "http://openweathermap.org/img/wn/" + response.daily[3].weather[0].icon + ".png";
+                var iconURL4 = "http://openweathermap.org/img/wn/" + response.daily[4].weather[0].icon + ".png";
+                // This displays icons to the appropriate
+                $("#wicon1").attr("src", iconURL0);
+                $("#wicon2").attr("src", iconURL1);
+                $("#wicon3").attr("src", iconURL2);
+                $("#wicon4").attr("src", iconURL3);
+                $("#wicon5").attr("src", iconURL4);
 
-                // This uses the lat and lon pulled from the above method to call desired weather data
-                $.ajax({
-                    url: gpsURL,
-                    method: "Get"
-                }).then(function (response) {
-                    console.log(response);
-                    var iconCode = response.current.weather[0].icon;
-                    var iconURL = "http://openweathermap.org/img/wn/" + iconCode + ".png";
-                    var fahrenheit = Math.round(((parseFloat(res.main.temp) - 273.15) * 1.80 + 32));
-                    var celcius = Math.round(parseFloat(res.main.temp) - 273.15);
-                    // Displays data onto designated elements
-                    $("#currentCity").text(res.name + "|" + (moment().format(" MMM. Do YY")));
-                    $("#description").text("Clouds: " + res.weather[0].description);
-                    $("#humidity").text("Humidity: " + res.main.humidity + "%");
-                    $("#windSpeed").text("Wind Speed: " + res.wind.speed);
-                    $("#fahrenheit").text("Temp: " + fahrenheit + degree + "F");
-                    $("#celcius").text("Temp: " + celcius + degree + "C");
-                    // Displaying data from second object
-                    $("#uv").text("UV Index: " + response.current.uvi);
-                    $("#wicon").attr("src", iconURL);
-
-                    // This Displays 5 day forecast for the week
-                    // This displays upcoming dates
-                    $("#date1").text(moment().add(1, 'days').format("MMM Do YY"));
-                    $("#date2").text(moment().add(2, 'days').format("MMM Do YY"));
-                    $("#date3").text(moment().add(3, 'days').format("MMM Do YY"));
-                    $("#date4").text(moment().add(4, 'days').format("MMM Do YY"));
-                    $("#date5").text(moment().add(5, 'days').format("MMM Do YY"));
-                    // Forecast for temp
-                    $("#temp1").text("Temperature: " + Math.round(((response.daily[0].temp.day) - 273.15) * 1.80 + 32) + degree);
-                    $("#temp2").text("Temperature: " + Math.round(((response.daily[1].temp.day) - 273.15) * 1.80 + 32) + degree);
-                    $("#temp3").text("Temperature: " + Math.round(((response.daily[2].temp.day) - 273.15) * 1.80 + 32) + degree);
-                    $("#temp4").text("Temperature: " + Math.round(((response.daily[3].temp.day) - 273.15) * 1.80 + 32) + degree);
-                    $("#temp5").text("Temperature: " + Math.round(((response.daily[4].temp.day) - 273.15) * 1.80 + 32) + degree);
-                    // Forecast for humidity
-                    $("#humidity1").text("Humidity: " + response.daily[0].humidity + "%");
-                    $("#humidity2").text("Humidity: " + response.daily[1].humidity + "%");
-                    $("#humidity3").text("Humidity: " + response.daily[2].humidity + "%");
-                    $("#humidity4").text("Humidity: " + response.daily[3].humidity + "%");
-                    $("#humidity5").text("Humidity: " + response.daily[4].humidity + "%");
-                    // This displays the weather icon for upcoming day
-                    // URLS to one call open weather api
-                    var iconURL0 = "http://openweathermap.org/img/wn/" + response.daily[0].weather[0].icon + ".png";
-                    var iconURL1 = "http://openweathermap.org/img/wn/" + response.daily[1].weather[0].icon + ".png";
-                    var iconURL2 = "http://openweathermap.org/img/wn/" + response.daily[2].weather[0].icon + ".png";
-                    var iconURL3 = "http://openweathermap.org/img/wn/" + response.daily[3].weather[0].icon + ".png";
-                    var iconURL4 = "http://openweathermap.org/img/wn/" + response.daily[4].weather[0].icon + ".png";
-                    // This displays icons to the appropriate
-                    $("#wicon1").attr("src", iconURL0);
-                    $("#wicon2").attr("src", iconURL1);
-                    $("#wicon3").attr("src", iconURL2);
-                    $("#wicon4").attr("src", iconURL3);
-                    $("#wicon5").attr("src", iconURL4);
-                })
+                var uvIndex = response.current.uvi;
+                if (uvIndex < 3) {
+                    $("#uv").addClass("low");
+                } else if (uvIndex < 6) {
+                    $("#uv").addClass("moderate");
+                } else if (uvIndex < 8) {
+                    $("#uv").addClass("high");
+                } else if (uvIndex < 11) {
+                    $("#uv").addClass("extreme");
+                }
             })
-        
+        })
+
         // This saves the users serached city to local storage
         storeCities();
 
     });
 });
-
-
